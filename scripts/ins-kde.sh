74fe5171ec89299e0e5c97bc78029400fa288be9
vetealdiablo <46105187+vetealdiablo@users.noreply.github.com>
2021-07-18 19:12:17 -0300

    Update ins-kde.sh

--- scripts/ins-kde.sh
+++ scripts/ins-kde.sh
@@ -10,11 +10,8 @@ locale-gen
 grub-install /dev/sdb
 grub-mkconfig -o /boot/grub/grub.cfg
 mkinitcpio -p linux
-useradd -m -g users -G wheel,audio,lp,optical,storage,video,games,power,scanner -s /bin/bash kio
 useradd -m -g users -G wheel,audio,lp,optical,storage,video,games,power,scanner -s /bin/bash cesc
-usermod -c "kio" kio
 usermod -c "Cesc" cesc
-passwd kio
 passwd cesc
 pacman -S xorg xorg-fonts-misc ttf-font-awesome adobe-source-code-pro-fonts sddm zsh zsh-completions nvidia nvidia-settings nvidia-utils networkmanager plasma konsole kate dolphin
 sudo nvidia-xconfig
