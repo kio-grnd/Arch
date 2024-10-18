faa0adac29629b8d06d52a04af5b4d71f4ea07be
vetealdiablo <46105187+vetealdiablo@users.noreply.github.com>
2024-10-18 05:35:41 -0300

    Update arch-install.sh

--- scripts/arch-install.sh
+++ scripts/arch-install.sh
@@ -3,23 +3,23 @@
 # -----------------------------
 # Variables generales del script
 # -----------------------------
-read -p "Introduce el nombre del disco (ejemplo: /dev/sda): " DISK
-read -p "Introduce el tamaño de la partición swap (ejemplo: 2G): " SWAPSIZE
+# read -p "Introduce el nombre del disco (ejemplo: /dev/sda): " DISK
+# read -p "Introduce el tamaño de la partición swap (ejemplo: 2G): " SWAPSIZE
 read -p "Introduce el nombre del host: " HOSTNAME
 read -p "Introduce tu nombre de usuario: " USERNAME
 
 # -----------------------------
 # Particionamiento
 # -----------------------------
-echo "Particionando el disco..."
-echo "Confirmando particiones..."
+# echo "Particionando el disco..."
+# echo "Confirmando particiones..."
 read -p "Introduce la partición raíz (ejemplo: /dev/sda1): " ROOT_PARTITION
 read -p "Introduce la partición swap (ejemplo: /dev/sda2): " SWAP_PARTITION
 
-mkfs.ext4 $ROOT_PARTITION
-mkswap $SWAP_PARTITION
-swapon $SWAP_PARTITION
-mount $ROOT_PARTITION /mnt
+# mkfs.ext4 $ROOT_PARTITION
+# mkswap $SWAP_PARTITION
+# swapon $SWAP_PARTITION
+# mount $ROOT_PARTITION /mnt
 
 # -----------------------------
 # Instalación base del sistema
