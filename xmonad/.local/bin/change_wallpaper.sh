dc90f8744cdd65815d2622fbb03ad57c0f1e94ef
vetealdiablo <46105187+vetealdiablo@users.noreply.github.com>
2024-10-22 03:18:50 -0300

    Update change_wallpaper.sh

--- xmonad/.local/bin/change_wallpaper.sh
+++ xmonad/.local/bin/change_wallpaper.sh
@@ -3,7 +3,7 @@
 # Cambiar el fondo de pantalla cada minuto
 while true; do
     # Cambia el fondo de pantalla usando imágenes de la carpeta especificada
-    feh --randomize --bg-fill ~/.config/backgrounds/*
+    feh --randomize --bg-fill ~/.config/wallpapers/*
 
     # Esperar 60 segundos antes de cambiar nuevamente
     sleep 60
