20de8ebda76ec6dd839833cb7ed9c506e5ac617e
vetealdiablo <46105187+vetealdiablo@users.noreply.github.com>
2024-10-24 04:29:23 -0300

    Update arch-install-2-xmonad.sh

--- scripts/arch-install-2-xmonad.sh
+++ scripts/arch-install-2-xmonad.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 # -----------------------------
-# Configuración del sistema
+# Establecer el teclado en español
 # -----------------------------
-read -p "Introduce el nombre del host: " HOSTNAME
-read -p "Introduce tu nombre de usuario: " USERNAME
+loadkeys es
+
+# -----------------------------
+# Preguntar al usuario por nombre de host y nombre de usuario
+# -----------------------------
+read -p "Introduce el nombre de host: " HOSTNAME
+read -p "Introduce el nombre de usuario: " USERNAME
+
+# -----------------------------
+# Configuración inicial del sistema
+# -----------------------------
 
 ln -sf /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
 hwclock --systohc
@@ -25,139 +34,19 @@ passwd $USERNAME
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
 # -----------------------------
-# Mostrar discos disponibles
+# Mostrar discos y particiones disponibles
 # -----------------------------
-echo "Discos disponibles:"
+
+echo "Mostrando discos y particiones disponibles:"
 lsblk
 
-# -----------------------------
-# Instalación de controladores NVIDIA
-# -----------------------------
-pacman -S nvidia nvidia-utils nvidia-settings --noconfirm
+echo "Usa el comando 'fdisk -l' o 'lsblk -f' para detalles adicionales."
+read -p "Presiona Enter para continuar..."
 
 # -----------------------------
-# Desactivar Nouveau
+# Instalación y configuración de GRUB
 # -----------------------------
-echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf
-echo "options nouveau modeset=0" >> /etc/modprobe.d/nouveau.conf
 
-# Crear un archivo de configuración para regenerar la inicialización
-echo "drm.modeset=0" >> /boot/loader/entries/arch.conf
-
-# -----------------------------
-# Instalación de Xorg
-# -----------------------------
-pacman -S xorg xorg-server xorg-xinit --noconfirm
-
-# -----------------------------
-# Instalación y configuración de sonido (PipeWire)
-# -----------------------------
-pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber alsa-utils --noconfirm
-systemctl --user enable wireplumber
-systemctl --user start wireplumber
-
-alsamixer
-
-# -----------------------------
-# Instalación de XMonad y Xmobar
-# -----------------------------
-pacman -S --noconfirm --needed xmonad xmonad-contrib xmobar
-
-# -----------------------------
-# Instalación de utilidades necesarias
-# -----------------------------
-pacman -S --noconfirm --needed kitty xterm dmenu rofi feh picom htop numlockx loupe lxappearance neovim git bat ranger ueberzug wget curl zsh zsh-completions xbindkeys rxvt-unicode ttf-bitstream-vera
-
-# -----------------------------
-# Instalación de compiladores y herramientas de desarrollo
-# -----------------------------
-pacman -S --noconfirm --needed git base-devel gcc make dkms linux-headers gd cmake python python-pip go rust 
-
-# -----------------------------
-# Instalación de soporte NTFS y herramientas de descompresión
-# -----------------------------
-pacman -S --noconfirm --needed ntfs-3g unzip p7zip
-
-# -----------------------------
-# Activar y configurar NetworkManager
-# -----------------------------
-pacman -S --noconfirm --needed networkmanager
-systemctl enable NetworkManager
-systemctl start NetworkManager
-
-# -----------------------------
-# Activar y configurar Bluetooth
-# -----------------------------
-pacman -S bluez bluez-utils --noconfirm
-
-# Habilitar el servicio Bluetooth
-systemctl enable bluetooth.service
-systemctl start bluetooth.service
-
-# Configurar para iniciar con bluetoothctl
-echo -e "power on\nagent on\ndefault-agent\n" | bluetoothctl
-
-# -----------------------------
-# Configuración del teclado para X11 (ES)
-# -----------------------------
-echo "Configurando el teclado en español para X11..."
-mkdir -p /etc/X11/xorg.conf.d/
-cat << EOF > /etc/X11/xorg.conf.d/00-keyboard.conf
-# Configuración del teclado en español para X11
-Section "InputClass"
-        Identifier "system-keyboard"
-        MatchIsKeyboard "on"
-        Option "XkbLayout" "es"
-        Option "XkbModel" "pc105"
-        Option "XkbOptions" "terminate:ctrl_alt_bksp"
-EndSection
-EOF
-
-# -----------------------------
-# Creación del archivo .xinitrc
-# -----------------------------
-echo "Creando el archivo .xinitrc..."
-cat << EOF > /home/$USERNAME/.xinitrc
-#!/bin/bash
-setxkbmap es
-xbindkeys & 
-numlockx on & 
-picom & 
-xsetroot -cursor_name left_ptr &
-exec xmonad
-EOF
-chmod +x /home/$USERNAME/.xinitrc
-chown $USERNAME:$USERNAME /home/$USERNAME/.xinitrc
-
-# -----------------------------
-# Copiar dotfiles a la carpeta home
-# -----------------------------
-echo "Clonando dotfiles desde GitHub..."
-git clone https://github.com/vetealdiablo/Arch.git /tmp/arch
-
-echo "Copiando dotfiles a /home/$USERNAME..."
-cp -r /tmp/arch/dotfiles/.* /home/$USERNAME/
-
-# Cambiar el propietario de los archivos copiados al usuario correspondiente
-chown -R $USERNAME:$USERNAME /home/$USERNAME/.*
-# Cambiar los permisos de los archivos copiados
-chmod -R u+rwX /home/$USERNAME/.*
-
-# Limpiar
-rm -rf /tmp/arch
-
-# -----------------------------
-# Instalación de Oh My Zsh y configuración de Zsh como shell predeterminada
-# -----------------------------
-echo "Instalando Oh My Zsh..."
-sudo -u $USERNAME sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Cambiar shell predeterminada a Zsh
-chsh -s /bin/zsh $USERNAME
-
-# -----------------------------
-# Elegir donde instalar GRUB
-# -----------------------------
 read -p "Introduce el dispositivo donde instalar GRUB (ejemplo: /dev/sda): " DISCO
 
 # Validar que el dispositivo sea correcto
@@ -194,8 +83,165 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # -----------------------------
+# Instalación de controladores NVIDIA y configuración de gráficos
+# -----------------------------
+
+pacman -S nvidia nvidia-utils nvidia-settings --noconfirm
+
+# Desactivar Nouveau
+echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf
+echo "options nouveau modeset=0" >> /etc/modprobe.d/nouveau.conf
+echo "drm.modeset=0" >> /boot/loader/entries/arch.conf
+
+# -----------------------------
+# Instalación de Xorg (servidor gráfico)
+# -----------------------------
+
+pacman -S xorg xorg-server xorg-xinit --noconfirm
+
+# -----------------------------
+# Configuración de sonido (PipeWire)
+# -----------------------------
+
+pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber alsa-utils --noconfirm
+systemctl --user enable wireplumber
+systemctl --user start wireplumber
+alsamixer
+
+# -----------------------------
+# Configuración de NetworkManager
+# -----------------------------
+
+pacman -S --noconfirm --needed networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+
+# -----------------------------
+# Instalación de XMonad y Xmobar
+# -----------------------------
+
+pacman -S --noconfirm --needed xmonad xmonad-contrib xmobar
+
+# -----------------------------
+# Instalación de utilidades esenciales
+# -----------------------------
+
+pacman -S --noconfirm --needed kitty xterm dmenu rofi feh picom htop numlockx loupe lxappearance neovim git bat ranger ueberzug wget curl zsh zsh-completions xbindkeys rxvt-unicode ttf-bitstream-vera
+
+# -----------------------------
+# Instalación de compiladores y herramientas de desarrollo
+# -----------------------------
+
+pacman -S --noconfirm --needed git base-devel gcc make dkms linux-headers gd cmake python python-pip go rust
+
+# -----------------------------
+# Instalación de soporte NTFS y herramientas de descompresión
+# -----------------------------
+
+pacman -S --noconfirm --needed ntfs-3g unzip p7zip
+
+# -----------------------------
+# Instalación y configuración de Bluetooth
+# -----------------------------
+
+pacman -S bluez bluez-utils --noconfirm
+systemctl enable bluetooth.service
+systemctl start bluetooth.service
+echo -e "power on\nagent on\ndefault-agent\n" | bluetoothctl
+
+# -----------------------------
+# Configuración del teclado para X11 (ES)
+# -----------------------------
+
+echo "Configurando el teclado en español para X11..."
+mkdir -p /etc/X11/xorg.conf.d/
+cat << EOF > /etc/X11/xorg.conf.d/00-keyboard.conf
+# Configuración del teclado en español para X11
+Section "InputClass"
+        Identifier "system-keyboard"
+        MatchIsKeyboard "on"
+        Option "XkbLayout" "es"
+        Option "XkbModel" "pc105"
+        Option "XkbOptions" "terminate:ctrl_alt_bksp"
+EndSection
+EOF
+
+# -----------------------------
+# Creación del archivo .xinitrc
+# -----------------------------
+
+echo "Creando el archivo .xinitrc..."
+cat << EOF > /home/$USERNAME/.xinitrc
+#!/bin/bash
+
+# ~/.xinitrc
+
+# Configuración del teclado
+setxkbmap es
+
+# Iniciar xbindkeys para atajos de teclado
+xbindkeys &
+
+# Cargar configuraciones gráficas de X11
+xrdb -merge ~/.Xresources &
+
+# Activar NumLock al inicio
+numlockx on &
+
+# Iniciar compositor gráfico (opcional)
+# picom &
+
+# Iniciar Polybar
+polybar &
+
+# Establecer cursor por defecto
+xsetroot -cursor_name left_ptr &
+
+# Iniciar xmonad
+exec xmonad
+EOF
+chmod +x /home/$USERNAME/.xinitrc
+chown $USERNAME:$USERNAME /home/$USERNAME/.xinitrc
+
+# -----------------------------
+# Copiar dotfiles al directorio home
+# -----------------------------
+
+echo "Clonando dotfiles desde GitHub..."
+git clone https://github.com/vetealdiablo/Arch.git /tmp/arch
+
+echo "Copiando dotfiles a /home/$USERNAME..."
+cp -r /tmp/arch/xmonad/* /tmp/arch/xmonad/.[!.]* /home/$USERNAME/
+
+chown -R $USERNAME:$USERNAME /home/$USERNAME/.*
+chmod -R u+rwX /home/$USERNAME/.*
+
+# Limpiar
+rm -rf /tmp/arch
+
+# -----------------------------
+# Instalación de Oh My Zsh para usuario y root
+# -----------------------------
+
+echo "Instalando Oh My Zsh para el usuario $USERNAME..."
+sudo -u $USERNAME sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Instalando Oh My Zsh para root..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Cambiar la shell predeterminada a Zsh para el usuario y root
+chsh -s /bin/zsh $USERNAME
+chsh -s /bin/zsh root
+
+# Recargar la shell para aplicar Zsh inmediatamente
+echo "Recargando shell para aplicar Zsh..."
+sudo -u $USERNAME zsh -c "source ~/.zshrc"
+zsh -c "source ~/.zshrc"
+
+# -----------------------------
 # Finalización
 # -----------------------------
+
 echo -e "\e[32mEl script ha finalizado correctamente.\e[0m"
 umount -R /mnt
 reboot
