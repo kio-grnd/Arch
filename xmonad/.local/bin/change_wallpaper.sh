73a2a51d59181689a58745c477e0da8b49d805bb
vetealdiablo <46105187+vetealdiablo@users.noreply.github.com>
2024-10-24 04:15:01 -0300

    Delete xmonad/.local/bin/change_wallpaper.sh

--- xmonad/.local/bin/change_wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Cambiar el fondo de pantalla de forma continua
-while true; do
-    # Cambia el fondo de pantalla usando imágenes de la carpeta especificada
-    feh --randomize --bg-fill ~/.config/wallpapers/*
-done
-
