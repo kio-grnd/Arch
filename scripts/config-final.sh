dcfe17d2023f72956ca6aaff0eb120df3d30e994
Kio <46105187+kio-grnd@users.noreply.github.com>
2024-11-08 17:47:34 -0300

    Create config-final.sh

---
+++ scripts/config-final.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Instalar yay desde AUR
+echo "Clonando y construyendo yay desde AUR..."
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
+makepkg -si --noconfirm
+cd ~
+rm -rf /tmp/yay
+
+# Instalar Zsh y establecerlo como shell predeterminado
+echo "Instalando Zsh y configurando como shell predeterminado..."
+pacman -S --noconfirm zsh
+chsh -s /bin/zsh
+
+# Instalar Oh-My-Zsh
+echo "Instalando Oh-My-Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Instalar Google Chrome
+echo "Instalando Google Chrome con yay..."
+yay -S --noconfirm google-chrome
+
+# Fin
+echo "La configuración final se ha completado con éxito."
